5bdc3469c88750c13e89c454da15961264eadddb
Saki-M-0831 <sakisaki0831@icloud.com>
2021-06-11 16:26:36 +0900

    credentials

--- config/environment.rb
+++ config/environment.rb
@@ -6,8 +6,8 @@ Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
   :user_name => 'apikey', # This is the string literal 'apikey', NOT the ID of your API key
-  :password => 'SG.WFBcWi90SMSR_NHp3UEX6Q.Lc-c7V0KcRfkW1wl7SEwrNRpNpEl_jwpeVHS183GXNA', # This is the secret sendgrid API key which was issued during API key creation
-  :domain => 'http://localhost:3005"',
+  :password => Rails.application.credentials.sendgrid[:api_key], # This is the secret sendgrid API key which was issued during API key creation
+  :domain => 'http://localhost:3005',
   :address => 'smtp.sendgrid.net',
   :port => 587,
   :authentication => :plain,
