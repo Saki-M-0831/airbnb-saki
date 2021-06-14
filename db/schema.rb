738fc34806dfdf637176c91309cc5764c961602d
Saki-M-0831 <sakisaki0831@icloud.com>
2021-06-14 17:36:38 +0900

    sns authentification

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_09_073104) do
+ActiveRecord::Schema.define(version: 2021_06_11_083405) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "picture"
@@ -27,6 +27,9 @@ ActiveRecord::Schema.define(version: 2021_06_09_073104) do
     t.datetime "confirmation_sent_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "provider"
+    t.string "uid"
+    t.string "image"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
