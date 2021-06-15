6f2c134d441f0ff68cf0a6c4257a5b482888e0d3
Saki-M-0831 <sakisaki0831@icloud.com>
2021-06-15 15:39:05 +0900

    [function]accommodation

--- app/controllers/accommodations_controller.rb
+++ app/controllers/accommodations_controller.rb
@@ -1,10 +1,25 @@
 class AccommodationsController < ApplicationController
   def new
+    @accommodation = Accommodation.new
+  end
+
+  def create
+    @accommodation = Accommodation.new(accommo_params)
+    if @accommodation.save
+      redirect_to root_url
+    else
+      render "new"
+    end
   end
 
   def edit
   end
 
   def index
+  end
+
+  private
+  def accommo_params
+    params.require(:accommodation).permit(:home_type, :room_typr, :accommodate, :bedroom_type, :bathroom_type, :price, :name, :summary, :has_tv, :has_kitchen, :has_internet, :has_heating, :has_ac, :location, :is_active)
   end
 end
