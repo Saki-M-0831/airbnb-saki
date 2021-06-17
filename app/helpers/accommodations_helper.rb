999c288dc9056cbe827eca7773a56d740edef7fc
Saki-M-0831 <sakisaki0831@icloud.com>
2021-06-17 15:45:46 +0900

    update

--- app/helpers/accommodations_helper.rb
+++ app/helpers/accommodations_helper.rb
@@ -4,4 +4,8 @@ module AccommodationsHelper
       content_tag :i, nil, class: "fa fa-check-circle"
     end
   end
+
+  def is_ready?(accommo)
+    accommo.photos.any? && !accommo.name.nil? && !accommo.price.nil? && !accommo.location.nil?
+  end
 end
