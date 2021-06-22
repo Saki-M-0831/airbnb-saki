6ca8913a8758951fa00b1a55612a7b368386fe8f
Saki-M-0831 <sakisaki0831@icloud.com>
2021-06-22 17:48:21 +0900

    [function]booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,14 +1,19 @@
 class BookingsController < ApplicationController
   def create
-    @booking = Booking.new(book_params)
-
+    @booking = current_user.bookings.new(book_params)
     if @booking.save
+      @booking.update(total_price: @booking.accommodation.price * (@booking.check_out - @booking.check_in))
+
+
       flash[:success] = "You have successfully booked this accommodation!!"
+    else
+      flash[:danger] = "You failed booking, please try again..."
     end
+    redirect_back(fallback_location: request.referer)
   end
 
   private
   def book_params
-    params.require(:booking).permit(:check_in, :check_out)
+    params.require(:booking).permit(:check_in, :check_out, :accommodation_id)
   end
 end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,4 @@
 class Booking < ApplicationRecord
-	belongs_to :user
-	belongs_to :accommodation
+  belongs_to :user
+  belongs_to :accommodation
 end
