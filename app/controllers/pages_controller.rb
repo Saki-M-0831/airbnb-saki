98e7ff5a968c16a5f94e6e49191028fb2e6f5b77
Saki-M-0831 <83812192+Saki-M-0831@users.noreply.github.com>
2021-06-25 16:40:51 +0900

    [layout]home (#22)
    * [layout]home
    
    * datepicker

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
   def home
+    @accommodations = Accommodation.where(is_active: true).paginate(page: params[:page], per_page: 6)
   end
 
   def about
