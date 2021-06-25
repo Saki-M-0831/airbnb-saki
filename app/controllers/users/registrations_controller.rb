dfbc8e99f966298af8a7dbfb630fcd8522a0dcf5
Saki-M-0831 <sakisaki0831@icloud.com>
2021-06-25 15:22:34 +0900

    change

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -23,6 +23,11 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # def update
   #   super
   # end
+  protected
+  def update_resource(resource, params)
+    resource.update_without_password(params)
+  end
+
 
   # DELETE /resource
   # def destroy
