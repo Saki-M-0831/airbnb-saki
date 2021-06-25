dd9c7f750fc0ad5216adadb70748b2f6297935b0
Saki-M-0831 <sakisaki0831@icloud.com>
2021-06-25 15:37:36 +0900

    ommittion

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -23,11 +23,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # def update
   #   super
   # end
-  protected
-  def update_resource(resource, params)
-    resource.update_without_password(params)
-  end
-
 
   # DELETE /resource
   # def destroy
