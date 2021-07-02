b2c88c32567492c4ebe76b7f459ec294be0fad47
Saki-M-0831 <sakisaki0831@icloud.com>
2021-07-02 19:17:59 +0900

    seeders

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+50.times do |n| 
+  user = User.new(
+    name: Faker::Movies::HarryPotter.character,
+    email: "email#{n+1}@email.com",
+    password: "password",
+    description: Faker::Movies::HarryPotter.quote
+  )
+  user.skip_confirmation!
+  user.save
+end
+
+users = User.all
+home_t = ["House", "Bed & Breakfast", "Apartment"]
+room_t = ["Private", "Shared", "Entire"]
+
+users.each do |user|
+  Accommodation.create!(
+    user_id: user.id,
+    home_type: home_t.sample,
+    room_type: room_t.sample,
+    accommodate: Faker::Number.within(range: 1..4),
+    bedroom_count: Faker::Number.within(range: 1..4),
+    bathroom_count: Faker::Number.within(range: 1..4),
+    price: Faker::Number.within(range: 50..1000),
+    name: Faker::Movies::HarryPotter.location,
+    summary: Faker::Movies::HarryPotter.quote,
+    location: Faker::Address.full_address,
+    is_active: false
+  )
+end
